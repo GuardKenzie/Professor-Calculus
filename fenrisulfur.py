74e126188c95d4d3f9fe219dc2df83dabe305ab0
Kenzie <tristanferrua@gmail.com>
2019-08-08 17:36:17 +0000

    Added setup and implemented role restrictions

--- fenrisulfur.py
+++ fenrisulfur.py
@@ -164,168 +164,188 @@ async def on_ready():
 
 @fenrir.event
 async def on_guild_join(guild):
-    cat = await guild.create_category("Fenrir")
-    channel = await guild.create_text_channel("events", category=cat)
-    await channel.send(content="Hello! I'm Fenrisúlfur or Fenrir for short. Nice to meet you :D\nThis is my events channel. Here I will post notifications for upcoming FC events!\nPlease type `"+prefix+"help` in chat to see what I'm capable of.")
+    for i in guild.text_channels:
+        try:
+            await i.send(content="Type `" +prefix+ "setup` to get started")
+            break
+        except discord.errors.Forbidden:
+            str(1)
+
+@fenrir.command()
+async def setup(ctx):
+    if isinstance(ctx.channel, discord.abc.GuildChannel):
+        cat = await ctx.guild.create_category("Fenrir")
+        channel = await ctx.guild.create_text_channel("events", category=cat)
+        await channel.send(content="Hello! I'm Fenrisúlfur or Fenrir for short. Nice to meet you :D\nThis is my events channel. Here I will post notifications for upcoming FC events!\nPlease type `"+prefix+"help` in chat to see what I'm capable of.")
+        await ctx.guild.create_role(name="Scheduler")
+        await channel.send(content="Assign the newly created role `Scheduler` to people you want to be able to schedule events.")
+
 
 @fenrir.command()
 async def events(ctx):
-    msg = discord.Embed(title="Scheduled events:", colour=discord.Colour.purple())
-    c.execute("SELECT * FROM events WHERE server_hash='{0}'".format(hash(ctx.guild)))
-    for i in c.fetchall():
-        numer = i[1]
-        name = i[3]
-        time = i[2].split(" ")[1]
-        date = i[2].split(" ")[0]
-        attendants = json.loads(i[5])
-        msg.add_field(name=name, value=date)
-        msg.add_field(name="Id:", value=str(numer), inline=True)
+    if isinstance(ctx.channel, discord.abc.GuildChannel):
+        msg = discord.Embed(title="Scheduled events:", colour=discord.Colour.purple())
+        c.execute("SELECT * FROM events WHERE server_hash='{0}'".format(hash(ctx.guild)))
+        for i in c.fetchall():
+            numer = i[1]
+            name = i[3]
+            time = i[2].split(" ")[1]
+            date = i[2].split(" ")[0]
+            attendants = json.loads(i[5])
+            msg.add_field(name=name, value=date)
+            msg.add_field(name="Id:", value=str(numer), inline=True)
 
-        # msg += "{0}. {1} on {2} at {3}:\n".format(numer, name, date, time)
-        attendees = 0
-        for name in attendants:
-            attendees += 1
-        msg.add_field(name="Party", value=str(attendees), inline=True)
-    await ctx.channel.send(embed=msg)
-    # await ctx.channel.send(content="Scheduled events and attendees:\n>>> "+msg)
-    print("1")
+            # msg += "{0}. {1} on {2} at {3}:\n".format(numer, name, date, time)
+            attendees = 0
+            for name in attendants:
+                attendees += 1
+            msg.add_field(name="Party", value=str(attendees), inline=True)
+        await ctx.channel.send(embed=msg)
+        # await ctx.channel.send(content="Scheduled events and attendees:\n>>> "+msg)
+        print("1")
 
 @fenrir.command()
 async def schedule(ctx, date, time, *, name):
-    if dcheck(date + " " + time):
-        c.execute("SELECT id FROM events WHERE server_hash={0}".format(hash(ctx.guild)))
-        i = 1
-        a = c.fetchall()
-        used = []
-        for entry in a:
-            used.append(entry[0])
-        print(used)
-        while i in used:
-            i += 1
-        c.execute("INSERT INTO events VALUES ('{0}', {1}, '{2}', '{3}', '', '[]')".format(hash(ctx.guild), i, pad(date + " " + time), name))
-        conn.commit()
-        await ctx.channel.send(content="Event `{0}` at `{1}` created with id `{2}`.".format(name, time + "` on `" + date, i))
-    else:
-        await ctx.channel.send(content="Please enter a valid date in the format `D/M/Y hour:minute`")
-    print("2")
+    if isinstance(ctx.channel, discord.abc.GuildChannel) and 'Scheduler' in [y.name for y in ctx.author.roles]:
+        if dcheck(date + " " + time):
+            c.execute("SELECT id FROM events WHERE server_hash={0}".format(hash(ctx.guild)))
+            i = 1
+            a = c.fetchall()
+            used = []
+            for entry in a:
+                used.append(entry[0])
+            print(used)
+            while i in used:
+                i += 1
+            c.execute("INSERT INTO events VALUES ('{0}', {1}, '{2}', '{3}', '', '[]')".format(hash(ctx.guild), i, pad(date + " " + time), name))
+            conn.commit()
+            await ctx.channel.send(content="Event `{0}` at `{1}` created with id `{2}`.".format(name, time + "` on `" + date, i))
+        else:
+            await ctx.channel.send(content="Please enter a valid date in the format `D/M/Y hour:minute`")
+        print("2")
 
 @fenrir.command()
 async def remove(ctx, *, numer):
-    print("5")
-    print(numer)
-    try:
-        numer = int(numer)
+    if isinstance(ctx.channel, discord.abc.GuildChannel) and 'Scheduler' in [y.name for y in ctx.author.roles]:
+        print("5")
+        print(numer)
+        try:
+            numer = int(numer)
 
-        if numer in allIds(c, hash(ctx.guild)):
-            i = ctx.args[0]
-            c.execute("DELETE FROM events WHERE id={0} AND server_hash='{1}';".format(int(numer), hash(ctx.guild)))
-            conn.commit()
-            await ctx.channel.send(content="Event with id `{0}` successfully deleted!".format(numer))
-        else:
-            await ctx.channel.send(content="That event does not exist!")
-    except TypeError:
-        await ctx.author.send(content="Usage: `remove [event id]` where `[event id]` is a number")
+            if numer in allIds(c, hash(ctx.guild)):
+                i = ctx.args[0]
+                c.execute("DELETE FROM events WHERE id={0} AND server_hash='{1}';".format(int(numer), hash(ctx.guild)))
+                conn.commit()
+                await ctx.channel.send(content="Event with id `{0}` successfully deleted!".format(numer))
+            else:
+                await ctx.channel.send(content="That event does not exist!")
+        except TypeError:
+            await ctx.author.send(content="Usage: `remove [event id]` where `[event id]` is a number")
     # TODO:
     # Fjarlægir event af dagskránni
 
 @fenrir.command()
 async def attend(ctx, *, numer):
-    try:
-        numer = int(numer)
-        author = ctx.message.author.display_name
+    if isinstance(ctx.channel, discord.abc.GuildChannel):
+        try:
+            numer = int(numer)
+            author = ctx.message.author.display_name
 
-        c.execute("SELECT * FROM events WHERE server_hash='{0}' AND id={1}".format(hash(ctx.guild), numer))
+            c.execute("SELECT * FROM events WHERE server_hash='{0}' AND id={1}".format(hash(ctx.guild), numer))
 
-        res = c.fetchone()
-        if res == None:
-            await ctx.channel.send(content="That event does not exist!")
-        else:
-            l = json.loads(res[5])
-
-            if author not in l:
-                l.append(author)
-                c.execute("UPDATE events SET people='{0}' WHERE server_hash='{1}' AND id={2}".format(json.dumps(l), hash(ctx.guild), numer))
-                conn.commit()
-                await ctx.channel.send(content="{0} is now attending `{1}`".format(author, res[3]))
+            res = c.fetchone()
+            if res == None:
+                await ctx.channel.send(content="That event does not exist!")
             else:
-                await ctx.channel.send(content="You are already attending that event!")
-    except TypeError:
-         await ctx.author.send(content="Usage: `attend [event id]` where `[event id]` is a number")
-    print("3")
-    # TODO:
-    # Bætir þér á listann af fólki sem mætir í event med id sem er gefið
+                l = json.loads(res[5])
+
+                if author not in l:
+                    l.append(author)
+                    c.execute("UPDATE events SET people='{0}' WHERE server_hash='{1}' AND id={2}".format(json.dumps(l), hash(ctx.guild), numer))
+                    conn.commit()
+                    await ctx.channel.send(content="{0} is now attending `{1}`".format(author, res[3]))
+                else:
+                    await ctx.channel.send(content="You are already attending that event!")
+        except TypeError:
+             await ctx.author.send(content="Usage: `attend [event id]` where `[event id]` is a number")
+        print("3")
+        # TODO:
+        # Bætir þér á listann af fólki sem mætir í event med id sem er gefið
 
 @fenrir.command()
 async def leave(ctx, *, numer):
-    try:
-        numer = int(numer)
-        author = ctx.message.author.display_name
+    if isinstance(ctx.channel, discord.abc.GuildChannel):
+        try:
+            numer = int(numer)
+            author = ctx.message.author.display_name
 
-        c.execute("SELECT * FROM events WHERE server_hash='{0}' AND id={1}".format(hash(ctx.guild), numer))
+            c.execute("SELECT * FROM events WHERE server_hash='{0}' AND id={1}".format(hash(ctx.guild), numer))
 
-        res = c.fetchone()
+            res = c.fetchone()
 
-        if res == None:
-            await ctx.channel.send(content="That event does not exist!")
-        else:
-            l = json.loads(res[5])
-            if author in l:
-                l.remove(author)
-                c.execute("UPDATE events SET people='{0}' WHERE server_hash='{1}' AND id={2}".format(json.dumps(l), hash(ctx.guild), numer))
-                conn.commit()
-                await ctx.channel.send(content="{0} is no longer attending `{1}`".format(author, res[3]))
+            if res == None:
+                await ctx.channel.send(content="That event does not exist!")
             else:
-                await ctx.channel.send(content="You are not attending that event!")
-    except TypeError:
-        await ctx.author.send(content="Usage: `leave [event id]` where `[event id]` is a number")
-    print("6")
+                l = json.loads(res[5])
+                if author in l:
+                    l.remove(author)
+                    c.execute("UPDATE events SET people='{0}' WHERE server_hash='{1}' AND id={2}".format(json.dumps(l), hash(ctx.guild), numer))
+                    conn.commit()
+                    await ctx.channel.send(content="{0} is no longer attending `{1}`".format(author, res[3]))
+                else:
+                    await ctx.channel.send(content="You are not attending that event!")
+        except TypeError:
+            await ctx.author.send(content="Usage: `leave [event id]` where `[event id]` is a number")
+        print("6")
 
 @fenrir.command()
 async def event(ctx, *, numer):
-    try:
-        numer = int(numer)
-        c.execute("SELECT * FROM events WHERE server_hash='{0}' AND id={1}".format(hash(ctx.guild), numer))
-        res = c.fetchone()
-        print(res)
-        if res == None:
-            await ctx.channel.send(content="That event does not exist!")
-        else:
-            attendees = ""
-            for name in json.loads(res[5]):
-                attendees += "\n" + name
-            if attendees == "":
-                attendees = "Empty :("
-            msg = discord.Embed(title=res[3], description=res[4], colour=discord.Colour.purple())
-            msg.add_field(name="When?", value=res[2], inline=False)
-            msg.add_field(name="Party:", value=attendees)
+    if isinstance(ctx.channel, discord.abc.GuildChannel):
+        try:
+            numer = int(numer)
+            c.execute("SELECT * FROM events WHERE server_hash='{0}' AND id={1}".format(hash(ctx.guild), numer))
+            res = c.fetchone()
+            print(res)
+            if res == None:
+                await ctx.channel.send(content="That event does not exist!")
+            else:
+                attendees = ""
+                for name in json.loads(res[5]):
+                    attendees += "\n" + name
+                if attendees == "":
+                    attendees = "Empty :("
+                msg = discord.Embed(title=res[3], description=res[4], colour=discord.Colour.purple())
+                msg.add_field(name="When?", value=res[2], inline=False)
+                msg.add_field(name="Party:", value=attendees)
 
-            # await ctx.channel.send(content=">>> *Name*: __**{0}**__\n*Date*: __{1}__\n*Description*: {2}\n*Attendees*:{3}".format(res[3],res[2],res[4],attendees))
-            await ctx.channel.send(embed=msg)
-    except TypeError:
-        await ctx.author.send(content="Usage: `event [event id]` where `[event id]` is a number")
-    print("4")
-    # TODO:
-    # Öfugt við attend
+                # await ctx.channel.send(content=">>> *Name*: __**{0}**__\n*Date*: __{1}__\n*Description*: {2}\n*Attendees*:{3}".format(res[3],res[2],res[4],attendees))
+                await ctx.channel.send(embed=msg)
+        except TypeError:
+            await ctx.author.send(content="Usage: `event [event id]` where `[event id]` is a number")
+        print("4")
+        # TODO:
+        # Öfugt við attend
 
 @fenrir.command()
 async def update(ctx, numer, what, *, instead):
-    try:
-        numer = int(numer)
-        if numer in allIds(c, hash(ctx.guild)):
-            valid = ["name", "date", "description", "people"]
-            if what in valid:
-                if (what == "date" and dcheck(instead)) or what != "date":
-                    if what == "date":
-                        what = pad(what)
-                    c.execute("UPDATE events SET {0}='{1}' WHERE server_hash='{2}' AND id={3}".format(what,instead,hash(ctx.guild),numer))
-                    conn.commit()
-                    await ctx.channel.send(content="Event `{0}`'s `{1}` updated to `{2}`".format(numer, what, instead))
-                else:
-                    await ctx.channel.send(content="Please enter a valid date in the format `D/M/Y hour:minute`")
-        else:
-            await ctx.channel.send(content="That event does not exist!")
-    except TypeError:
-        await ctx.author.send(content="Usage: `Update: [event id] [update catagory] [new value]` where `[event id]` is a number and Valid update catagories are\n```name\ndate\ndescription\npeople (format: \"['name1', 'name2',...]\")```")
+    if isinstance(ctx.channel, discord.abc.GuildChannel) and 'Scheduler' in [y.name for y in ctx.author.roles]:
+        try:
+            numer = int(numer)
+            if numer in allIds(c, hash(ctx.guild)):
+                valid = ["name", "date", "description", "people"]
+                if what in valid:
+                    if (what == "date" and dcheck(instead)) or what != "date":
+                        if what == "date":
+                            what = pad(what)
+                        c.execute("UPDATE events SET {0}='{1}' WHERE server_hash='{2}' AND id={3}".format(what,instead,hash(ctx.guild),numer))
+                        conn.commit()
+                        await ctx.channel.send(content="Event `{0}`'s `{1}` updated to `{2}`".format(numer, what, instead))
+                    else:
+                        await ctx.channel.send(content="Please enter a valid date in the format `D/M/Y hour:minute`")
+            else:
+                await ctx.channel.send(content="That event does not exist!")
+        except TypeError:
+            await ctx.author.send(content="Usage: `Update: [event id] [update catagory] [new value]` where `[event id]` is a number and Valid update catagories are\n```name\ndate\ndescription\npeople (format: \"['name1', 'name2',...]\")```")
 
 @fenrir.command()
 async def help(ctx, *, cmd="none"):
@@ -374,17 +394,9 @@ async def help(ctx, *, cmd="none"):
         else:
             await ctx.author.send(content="Unrecognised command")
 
-@fenrir.command()
-async def nei(ctx):
-    await fenrir.logout()
-
-@fenrir.command()
-async def aaaa(ctx):
-    msg = discord.Embed(title="Title", description="test test test", colour=discord.Colour.purple())
-    msg.add_field(name="test1", value="jskofs", inline=False)
-    msg.add_field(name="test2", value="jskofs", inline=True)
-    msg.add_field(name="test3", value="jskofs", inline=True)
-    await ctx.channel.send(embed=msg)
+# @fenrir.command()
+# async def nei(ctx):
+#     await fenrir.logout()
 
 @fenrir.event
 async def on_command_error(ctx, error):
