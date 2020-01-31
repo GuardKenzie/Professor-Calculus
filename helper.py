cdbba6cf9c8abf07307bb8f85341706a904d1244
Kenzie <tristanferrua@gmail.com>
2020-01-31 15:57:04 +0000

    31 Jan. 2020

--- helper.py
+++ helper.py
@@ -8,14 +8,6 @@ def helpCmd(prefix, cmd):
         msg.add_field(name="leave", value="Leave an event", inline=False)
         msg.add_field(name="update", value="Updates a scheduled event", inline=False)
         msg.add_field(name="eyebleach", value="Produces some eyebleach", inline=False)
-        msg.add_field(name="cringe", value="Produces some cringe", inline=False)
-        msg.add_field(name="drinkbleach", value="You die.", inline=False)
-        msg.add_field(name="chill", value="Joins voice and plays some Lo-Fi", inline=False)
-        msg.add_field(name="stress", value="Stops playing music.", inline=False)
-        msg.add_field(name="volume", value="Sets lofi volume.", inline=False)
-        msg.add_field(name="smswig", value="Take a swig of Spriggan Mead!", inline=False)
-        msg.add_field(name="smboard", value="Take a look at the leaderboards.", inline=False)
-        msg.add_field(name="me", value="Sets information like your birthday.", inline=False)
     else:
         if cmd == "schedule":
             msg = discord.Embed(title="schedule [event date (Format: 'DD/MM/YYYY hh:mm' or TBD)] [event name]")
@@ -43,30 +35,6 @@ def helpCmd(prefix, cmd):
         elif cmd == "eyebleach":
             msg = discord.Embed(title="eyebleach")
             msg.add_field(name="\u200b", value="Produces some eyebleach", inline=False)
-        elif cmd == "cringe":
-            msg = discord.Embed(title="cringe")
-            msg.add_field(name="\u200b", value="Produces some cringe (thanks Elath'a)", inline=False)
-        elif cmd == "drinkbleach":
-            msg = discord.Embed(title="drinkbleach")
-            msg.add_field(name="\u200b", value="Kills you.", inline=False)
-        elif cmd == "chill":
-            msg = discord.Embed(title="chill")
-            msg.add_field(name="\u200b", value="The bot joins your voice channel and starts playing some chill tunes.", inline=False)
-        elif cmd == "stress":
-            msg = discord.Embed(title="stress")
-            msg.add_field(name="\u200b", value="Bot stops playing music and leaves voice.", inline=False)
-        elif cmd == "volume":
-            msg = discord.Embed(title="volume [volume]")
-            msg.add_field(name="[volume]", value="a number from 0-100", inline=False)
-        elif cmd == "smswig":
-            msg = discord.Embed(title="smswig")
-            msg.add_field(name="\u200b", value="Take a swig of Spriggan Mead and get a complimentary misfortune cookie.", inline=False)
-        elif cmd == "smboard":
-            msg = discord.Embed(title="smboard")
-            msg.add_field(name="\u200b", value="prints the leaderboard for Spriggan Mead.", inline=False)
-        elif cmd == "me":
-            msg = discord.Embed(title="me birthday [date]")
-            msg.add_field(name="[date]", value="Your birthday DD/MM/YYY", inline=False)
         else:
             return -1
     return msg
