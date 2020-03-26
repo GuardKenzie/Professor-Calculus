baca5c649bc21346e7b656cb7ffc74ecf8cfaa51
Kenzie <tristanferrua@gmail.com>
2020-03-26 11:21:59 +0000

    Update events.py

--- events.py
+++ events.py
@@ -207,7 +207,11 @@ class Events():
     def getEvent(self, eventNumber):
         eventId = self.getEventId(eventNumber)
         self.c.execute("SELECT * FROM events WHERE server_hash=? AND id=?", (self.guildHash,eventId))
-        return event_class(self.c.fetchone())
+        res = self.c.fetchone()
+        if res is None:
+            return None
+        else:
+            return event_class(self.c.fetchone())
 
     def getEventId(self, eventNumber):
         # Takes event index and returns id
